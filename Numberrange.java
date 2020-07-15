d2907eb05fa77ae51af22149ad2b14ef3252926d
Prathamesh2803 <68282464+Prathamesh2803@users.noreply.github.com>
2020-07-15 15:53:09 +0530

    Add files via upload

---
+++ Numberrange.java
@@ -0,0 +1,15 @@
+package project1;
+
+public class Numberrange {
+	public static void main(String args[]) {
+		System.out.println("Number: ");		
+		for (int i=10; i<=100; i++) { 
+			if (i!=100)
+				System.out.print(i +", ");	
+			else
+				System.out.print(i);				
+		}			
+				
+  }
+
+}
